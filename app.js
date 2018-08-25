cc6fee364ac5f698f8e321f6c2c1912b173bcf0e
Carey Johnson <johnson.careyj@gmail.com>
2018-08-24 21:42:52 -0700

    added routes

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
-const db = require('./config/keys');
 
+
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
+const bodyParser = require('body-parser');
+
+// connect to MongoDB using mongoose
 mongoose
-  .connect(db)
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+.connect("db")
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
   
-app.get('/', (req, res) => res.send("Hello Carey!"));
+
+app.get('/', (req, res) => res.send("Hello Baby!"));
+app.get('/test', (req, res) => res.send("Hello BRO!"));
+app.use("/api/users", users);
+app.use("/api/events", events);
+app.use(bodyParser.urlencoded({extented:false}));
+app.use(bodyParser.json());
 // tell express what port to run on
 const port = process.env.PORT || 5000;
 
-
-app.listen(port, () => console.log(`Server is running on port:${port}`));
+// tell Express to start a socket and listen for connections on the path.
+app.listen(port, () => console.log(`Server is running on port ${port}`));
