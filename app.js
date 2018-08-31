13c1d60a8c0338aa9b96759ab41807251166b754
Carey Johnson <johnson.careyj@gmail.com>
2018-08-30 21:19:45 -0700

    added comments

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 
-
-const users = require('./routes/api/users');
-const events = require('./routes/api/events');
+// imported routes
+const users = require("./routes/api/users");
+const events = require("./routes/api/events");
 const bodyParser = require('body-parser');
 
 // connect to MongoDB using mongoose
@@ -13,12 +13,14 @@ mongoose
 .connect("db")
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
-  
+
+// tells express to use these new routes
+app.use("/api/users", users);
+app.use("/api/events", events);
 
 app.get('/', (req, res) => res.send("Hello Baby!"));
 app.get('/test', (req, res) => res.send("Hello BRO!"));
-app.use("/api/users", users);
-app.use("/api/events", events);
+
 app.use(bodyParser.urlencoded({extented:false}));
 app.use(bodyParser.json());
 // tell express what port to run on
