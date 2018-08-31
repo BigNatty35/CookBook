9819100da722de3af901cffcbab2066abfc2ed80
Carey Johnson <johnson.careyj@gmail.com>
2018-08-30 21:31:32 -0700

    explain what the routes are doing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+// tells express router to make a get request to the test route.
+// the response is a json object with a message.
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 module.exports = router;
