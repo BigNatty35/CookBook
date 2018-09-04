8851d7cca9d5549924bdcde0079cd6d1fb9672b4
Carey Johnson <johnson.careyj@gmail.com>
2018-09-03 22:06:17 -0700

    added RecipeSchema details

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  
+  directions: {
+    type: String,
+    required: true
+  }
+});
