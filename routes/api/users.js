7fd8e282413c736809f7167d58596d4382a78581
Carey Johnson <johnson.careyj@gmail.com>
2018-09-05 22:10:06 -0700

    imported jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jsonwebtoken = require('jsonwebtoken');
 // tells express router to make a get request to the test route.
 // the response is a json object with a message.
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
