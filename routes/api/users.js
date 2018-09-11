b70d6bca9719d7dae1da0fe911f130f4dc2bb1e1
Carey Johnson <johnson.careyj@gmail.com>
2018-09-10 22:37:41 -0700

    changed the router

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jsonwebtoken = require('jsonwebtoken');
+
+const router = express.Router();
 // tells express router to make a get request to the test route.
 // the response is a json object with a message.
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
