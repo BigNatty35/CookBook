dc6af3435dcb21ca8131c57890c2d7043781d095
Carey Johnson <johnson.careyj@gmail.com>
2018-09-17 21:23:06 -0700

    updated files

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 // tells express router to make a get request to the test route.
 // the response is a json object with a message.
-router.get("/test", (req, res) => res.json({ msg: "This is the events route" }));
+router.get("/test", (req, res) => res.json({ msg: "This is the entry route" }));
 
 module.exports = router;
