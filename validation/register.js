8a75805cf46db11ac08afd81d4830e6a6ad94b1c
Carey Johnson <johnson.careyj@gmail.com>
2018-09-20 23:03:08 -0700

    added comment to explain register

--- validation/register.js
+++ validation/register.js
@@ -4,6 +4,7 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  //this constructs the new user object
   data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
